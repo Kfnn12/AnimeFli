9eb4c9689b94f99080debec0775afd748e2e06df
Hassan Ayomide <funyem1710@gmail.com>
2023-03-14 01:06:51 +0100

    search link added for mobile

--- src/components/Header/MobileHeader.tsx
+++ src/components/Header/MobileHeader.tsx
@@ -1,6 +1,5 @@
 import { faBars, faList, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 import { Link } from "react-router-dom";
 import "twin.macro";
 
@@ -8,15 +7,17 @@ export default function MobileHeader() {
   return (
     <header tw="flex relative  w-full  bg-transparent  z-[1000] px-[25px] sm:px-[64px] py-[24px] items-center justify-between">
       <div tw="flex items-center gap-4">
-      <div tw="">
-        <FontAwesomeIcon size="xl" icon={faBars}/>
-      </div>
-      <Link to="/" tw="text-orange2 sm:text-2xl">
-        Logo
-      </Link>
+        <div tw="">
+          <FontAwesomeIcon size="xl" icon={faBars} />
+        </div>
+        <Link to="/" tw="text-orange2 sm:text-2xl">
+          Logo
+        </Link>
       </div>
       <div>
-        <FontAwesomeIcon icon={faSearch}/>
+        <a href="search">
+          <FontAwesomeIcon icon={faSearch} />
+        </a>
       </div>
     </header>
   );
