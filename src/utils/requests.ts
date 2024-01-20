9c9e3978978b751cbf8ee060c683e964d51c953f
Kfnn12 <132144405+Kfnn12@users.noreply.github.com>
2024-01-20 12:53:34 +0800

    Update requests.ts

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -2,9 +2,9 @@ const API_KEY = "f7148ae89d27521674f16516d92bc883"
 const BASE_URL = "https://api.themoviedb.org/3"
 
 const requests = {
-  fetchPopular: `https://gogoanime.consumet.org/popular`,
-  fetchRecents: 'https://gogoanime.consumet.org/recent-release',
-  fetchMovies: 'https://gogoanime.consumet.org/anime-movies',
-  fetchTopAiring: 'https://gogoanime.consumet.org/top-airing'
+  fetchPopular: `https://gogoanime.consumet.stream/popular`,
+  fetchRecents: 'https://gogoanime.consumet.stream/recent-release',
+  fetchMovies: 'https://gogoanime.consumet.stream/anime-movies',
+  fetchTopAiring: 'https://gogoanime.consumet.stream/top-airing'
 }
 export default requests;
